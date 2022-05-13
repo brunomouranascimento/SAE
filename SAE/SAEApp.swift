14780abf0a12196e7021a2c7d161dd3f224abf86
Bruno Nascimento <bruno.bmn@gmail.com>
2022-05-13 17:42:04 -0300

    Initial Commit

---
+++ SAE/SAEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAEApp.swift
+//  SAE
+//
+//  Created by Bruno Nascimento on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SAEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
